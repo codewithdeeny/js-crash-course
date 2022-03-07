a46da2f0fe9c58862e5de5a312c368783a0e8bbc
Deeny <codewithdeeny@users.noreply.github.com>
2022-03-07 14:37:41 +0100

    starting js class with clever programmer

---
+++ home.js
@@ -0,0 +1,16 @@
+console.log("Whats good?");
+//alert("Currently unavailable");
+
+// this double forward slash is used to write comment in javascript
+
+//variables
+//document.getElementById("someText").innerHTML = "Go hard or go home";
+//var age = prompt("what is your name");
+
+//numbers in js
+//var num1 = 10;
+//num1++;
+//console.log(num1 % 6);
+var num1 = 30;
+num1 = num1 += 40;
+console.log(num1);
